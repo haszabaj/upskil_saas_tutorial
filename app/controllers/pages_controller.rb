d078bec088e22a1b59d98cd007df35fd3ace18f8
Agata Malkiewicz <hushabye92@gmail.com>
2016-12-26 12:52:37 +0000

    Added subpages for the blog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,13 @@ class PagesController <ApplicationController
     def what
     end
     
+    def learn1
+    end
+    
+    def learn2
+    end
+    
+    def learn3
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     get 'what', to: 'pages#what'
+    get 'learn1', to: 'pages#learn1'
+    get 'learn2', to: 'pages#learn2'
+    get 'learn3', to: 'pages#learn3'
 end
